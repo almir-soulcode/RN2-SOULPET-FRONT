e077d518883e84053988bbcd7664cd148dc1c39c
almir-soulcode <jose.almir@soulcode.com>
2024-08-06 11:59:10 -0300

    aula0608: listagem de clientes; inserção de clientes;

---
+++ src/api/clientes.js
@@ -0,0 +1,15 @@
+// Este arquivo possui funções para realizar
+// as operações do CRUD de Clientes
+import axios from "axios";
+
+export async function getClientes() {
+    const response = await axios.get("http://localhost:3000/clientes");
+    // Dentro de 'data' está o JSON de resposta do back-end
+    return response.data;
+}
+
+export async function addCliente(data) {
+    // O 2º parâmetro do post é corpo da requisição
+    const response = await axios.post("http://localhost:3000/clientes", data);
+    return response.data;
+}
--- src/pages/Clientes.jsx
+++ src/pages/Clientes.jsx
@@ -1,7 +1,22 @@
-import { Button } from "react-bootstrap";
+import { Button, Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { getClientes } from "../api/clientes";
+import { useEffect, useState } from "react";
+import Loader from "../components/Loader";
 
 function Clientes() {
+  const [clientes, setClientes] = useState(null);
+
+  function carregarClientes() {
+    getClientes().then((dados) => {
+      setClientes(dados);
+    });
+  }
+
+  useEffect(() => {
+    carregarClientes();
+  }, []);
+
   return (
     <main className="mt-4 container">
       <h1>Clientes</h1>
@@ -9,6 +24,35 @@ function Clientes() {
         Adicionar Cliente
       </Button>
       <hr />
+      {clientes ? (
+        <Table>
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>E-mail</th>
+              <th>Telefone</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clientes.map((cliente) => {
+              return (
+                <tr key={cliente.id}>
+                  <td>{cliente.nome}</td>
+                  <td>{cliente.email}</td>
+                  <td>{cliente.telefone}</td>
+                  <td>
+                    <Button variant="danger" size="sm">Excluir</Button>
+                    <Button size="sm">Editar</Button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      ) : (
+        <Loader />
+      )}
     </main>
   );
 }
